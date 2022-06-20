1b14289fadd7430d1d92426659baed32a2834bc6
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-20 14:31:07 +0200

    added contact details

--- hubapp/views.py
+++ hubapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+
     return render(request, 'hubapp/index.html')
+
+def contact(request):
+
+    return render(request, 'hubapp/contact.html')    
