d2ab92dcf24b9c5a15bda8e39ef6c257a6ce4683
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-21 22:22:28 +0200

    added login details and included urls

--- hubapp/models.py
+++ hubapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    
