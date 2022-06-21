33b754d733e20d0ee1512a2721c4779c93a9e033
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-21 22:30:20 +0200

    added signup details

--- hubapp/urls.py
+++ hubapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('home/', views.index, name='home'),
+    path('signup/', views.sign_up, name='signup'),
     path('contact/', views.contact, name='contact'),
     path('resources/', views.resources, name='resources'),
     path('community/', views.community, name='community'),
--- hubapp/views.py
+++ hubapp/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 def index(request):
 
     return render(request, 'hubapp/index.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+    
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        # password1 = request.POST['password1']
+        image = request.FILES['image']
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        if user:
+            messages.success(request,'Profile Created Please Login')
+            return redirect('login')
+    else:  
+        return render(request, 'registration/signup.html', {})     
 
 def contact(request):
 
