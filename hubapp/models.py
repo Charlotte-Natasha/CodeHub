15d23a482e9905b5213cb5099f9eef8cea369244
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-22 13:28:48 +0200

    added profile and media details

--- hubapp/models.py
+++ hubapp/models.py
@@ -7,8 +7,22 @@ from django.dispatch import receiver
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_picture = models.ImageField(null=True, blank=True, upload_to='profilepics')
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete() 
