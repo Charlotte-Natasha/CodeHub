a8991b93565a464c1008b0cf4f328377fe15e64d
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-22 15:31:09 +0200

    created comment model

--- hubapp/models.py
+++ hubapp/models.py
@@ -45,3 +45,22 @@ def save_user_profile(sender, instance, **kwargs):
 
 post_save.connect(create_user_profile, sender=User)
 post_save.connect(save_user_profile, sender=User)
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+
+	def user_comment_post(sender, instance, *args, **kwargs):
+		comment = instance
+		post = comment.post
+		text_preview = comment.body[:90]
+		sender = comment.user
+
+
+	def user_del_comment_post(sender, instance, *args, **kwargs):
+		like = instance
+		post = like.post
+		sender = like.user
+
