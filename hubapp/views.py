ad1ccc6a0c821399c0b9610c5211ccd9db1498d6
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-23 21:30:04 +0200

    implemented comment details

--- hubapp/views.py
+++ hubapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import *
 from .forms import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="/login")
 def index(request):
 
     return render(request, 'hubapp/index.html')
