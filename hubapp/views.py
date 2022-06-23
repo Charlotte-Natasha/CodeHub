3d3c071c3a4359ee09f47fd73a6b86864cfb820c
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-23 20:39:48 +0200

    implemented profile authentication

--- hubapp/views.py
+++ hubapp/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
-from django.urls import reverse
-from django.template import loader
 from .models import *
 from .forms import *
 
